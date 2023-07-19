bf2cd983c6d92c535ac7f49b9597e9d44dc820df
Janne Immonen <immonenjanne@icloud.com>
2023-07-19 21:29:54 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [target, setTarget] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+
+  const checkGuess = () => {
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+      setMessage('Please enter a valid number between 1 and 100.');
+      return;
+    }
+    const numGuess = parseInt(guess);
+    if (numGuess > target) {
+      setMessage('Too high!');
+    } else if (numGuess < target) {
+      setMessage('Too low!');
+    } else {
+      setMessage('You got it! Press "Reset" to play again.');
+    }
+  };
+
+  const resetGame = () => {
+    setTarget(Math.floor(Math.random() * 100) + 1);
+    setGuess('');
+    setMessage('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Guess a number between 1 and 100</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setGuess(text)}
+        value={guess}
+        keyboardType="number-pad"
+      />
+      <Button onPress={checkGuess} title="Submit Guess" />
+      <Button onPress={resetGame} title="Reset" color="red" />
+      <Text>{message}</Text>
     </View>
   );
 }
@@ -13,8 +46,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
   },
 });
